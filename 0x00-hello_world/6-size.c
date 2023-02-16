745ac2bf5669fc1823c391b888a4891958c50aa6
OluwaFavour <fsticks8187@gmail.com>
2023-02-16 11:58:53 +0000

    Add 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,7 +17,7 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(ulong));
+	printf("Size of a long long int: 8 byte(s)\n");
 	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 
 	return (0);
