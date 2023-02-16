4c0486aad9edd2701391b6ad9c95b3ec986da1ec
OluwaFavour <fsticks8187@gmail.com>
2023-02-16 10:38:43 +0000

    Add 0-preprocessor

---
+++ 0x00-hello_world/test.c
@@ -0,0 +1,13 @@
+/* test.c */
+#include <stdio.h>
+
+/**
+ * main - This is where the main script executes
+ * Return: Returns 0 if the execution is successful
+ */
+int main(void)
+{
+	printf("Hello World");
+
+	return (0);
+}
