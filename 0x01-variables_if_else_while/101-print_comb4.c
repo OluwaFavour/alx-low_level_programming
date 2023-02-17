3b127c7190cb2ab982432ee06dc7c6d72e208c21
OluwaFavour <fsticks8187@gmail.com>
2023-02-17 11:07:04 +0000

    Add 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+/* 101-print_comb4.c */
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c, i;
+
+	a = 48; /* ASCII DEC code for '0' */
+	i = 57; /* ASCII DEC code for '9' */
+	for (; a < i - 1; a++)
+	{
+		for (b = a + 1; b < i; b++)
+		{
+			for (c = b + 1; c <= i; c++)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+				if (a < i - 1 || b < i || c <= i)
+				{
+					putchar(44); /* Print comma */
+					putchar(32); /* Print space */
+				}
+			}
+		}
+	}
+	putchar(10); /* Print new line */
+
+	return (0);
+}
