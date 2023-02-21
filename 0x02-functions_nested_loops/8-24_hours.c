5f609ab8a424bf4389107da7e6a379505af22edd
OluwaFavour <fsticks8187@gmail.com>
2023-02-21 21:45:17 +0000

    Add 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * jack_bauer - Prints the time from 00:00 to 23:59
+  */
+void jack_bauer(void)
+{
+	int hours, minutes;
+
+	while (hours < 24)
+	{
+		while (minutes < 60)
+		{
+			_putchar('0' + (hours / 10));
+			_putchar('0' + (hours % 10));
+			_putchar(':');
+			_putchar('0' + (minutes / 10));
+			_putchar('0' + (minutes % 10));
+			_putchar('\n');
+			minutes++;
+		}
+		minutes = 0;
+		hours++;
+	}
+}
