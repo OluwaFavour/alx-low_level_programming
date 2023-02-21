92bdf5c3a849727e7b31a87b0c54677ecce51b9b
OluwaFavour <fsticks8187@gmail.com>
2023-02-21 11:36:29 +0000

    Update main.h and add 2-print_alphabet.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+/* 2-print_alphabet_x10 */
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - Print the english alphabets in lowercase 10 times
+  */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
