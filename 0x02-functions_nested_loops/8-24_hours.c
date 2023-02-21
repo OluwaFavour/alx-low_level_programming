33140fd7fe86e0d7f0f9ee0dc9b625e972f502dc
OluwaFavour <fsticks8187@gmail.com>
2023-02-21 21:50:11 +0000

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,11 +11,11 @@ void jack_bauer(void)
 	{
 		while (minutes < 60)
 		{
-			_putchar('0' + (hours / 10));
-			_putchar('0' + (hours % 10));
+			_putchar('0' + hours / 10);
+			_putchar('0' + hours % 10);
 			_putchar(':');
-			_putchar('0' + (minutes / 10));
-			_putchar('0' + (minutes % 10));
+			_putchar('0' + minutes / 10);
+			_putchar('0' + minutes % 10);
 			_putchar('\n');
 			minutes++;
 		}
