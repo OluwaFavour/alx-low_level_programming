c4c8695d7ae22eeda93ff45a94f65488ccfd121b
OluwaFavour <fsticks8187@gmail.com>
2023-02-23 13:36:28 +0000

    Add 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * main - prints the largest prime factor of 612852475143
+  *
+  * Return - Always 0 (Success)
+  */
+int main(void)
+{
+	long divisor, numerator;
+
+	divisor = 2;
+	numerator = 612852475143;
+	while (numerator != 1)
+	{
+		if (numerator % divisor != 0)
+			divisor++;
+		else
+			numerator /= divisor;
+	}
+	printf("%ld\n", divisor);
+
+	return (0);
+}
