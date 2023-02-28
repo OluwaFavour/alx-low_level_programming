9b7566711bae1f05b802f71e26d50ec52d460329
OluwaFavour <fsticks8187@gmail.com>
2023-02-28 19:54:32 +0000

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,13 +2,13 @@
 
 /**
   * puts2 - prints every second character in a string
-  * @str - String to print character from
+  * @str: String to print character from
   */
 void puts2(char *str)
 {
 	char *p = str;
 
-	while (*p)
+	while (*p != '\0' && *(p + 1) != '\0')
 	{
 		_putchar(*p);
 		p += 2;
