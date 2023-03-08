f3d2660b61269e48e1e2cf425e134f19d00379f3
OluwaFavour <fsticks8187@gmail.com>
2023-03-08 21:48:26 +0000

    Add 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+int is_palindrome_helper(char *s, int start, int end);
 /**
   * is_palindrome - Checks if a string is a palindrome
   * @s: String to check
@@ -8,19 +9,14 @@
   */
 int is_palindrome(char *s)
 {
-	int i, j, len;
+	int len;
 
 	len = _strlen_recursion(s);
 
 	if (len == 0)
 		return (1);
-	for (i = 0, j = len - 1; i < j; i++, j--)
-	{
-		if (s[i] != s[j])
-			return (0);
-	}
 
-	return (1);
+	return (is_palindrome_helper(s, 0, len - 1));
 }
 
 /**
@@ -34,4 +30,23 @@ int _strlen_recursion(char *s)
 	if (*(s + 0) == '\0')
 		return (0);
 	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * is_palindrome_helper - Recursive helper function for is_palindrome
+ * @s: String to check
+ * @start: Starting index of the substring to check
+ * @end: Ending index of the substring to check
+ *
+ * Return: Returns 1 if substring is a palindrome and 0 if not
+ */
+int is_palindrome_helper(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+
+	if (s[start] != s[end])
+		return (0);
+
+	return (is_palindrome_helper(s, start + 1, end - 1));
 }
