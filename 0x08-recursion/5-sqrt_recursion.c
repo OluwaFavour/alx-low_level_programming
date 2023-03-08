5aa0d632ab716bfb2cd4dc5c69bba4620611c5e5
OluwaFavour <fsticks8187@gmail.com>
2023-03-08 20:39:19 +0000

    Add 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int calc_sqrt(int n, int start, int end);
+int calc_sqrt(int n, int start);
 /**
   * _sqrt_recursion - Returns the natural square root of a number
   * @n: Number to compute
@@ -14,37 +14,21 @@ int _sqrt_recursion(int n)
 		return (-1);
 	if (n == 0 || n == 1)
 		return (n);
-	return (calc_sqrt(n, 1, n));
+	return (calc_sqrt(n, 1));
 }
 
 /**
   * calc_sqrt - Calculates the square root
   * @n: Number to calculate
   * @start: Starting point
-  * @end: Ending point
   *
   * Return: Returns the square root
   */
-int calc_sqrt(int n, int start, int end)
+int calc_sqrt(int n, int start)
 {
-	int mid, sq, res;
-
-	if (start > end)
+	if (n == start * start)
+		return (start);
+	if (start >= (n / 2))
 		return (-1);
-
-	mid = start + (end - start) / 2;
-	sq = mid * mid;
-	if (sq == n)
-	{
-		return (mid);
-	}
-	else if (sq > n)
-	{
-		return (calc_sqrt(n, start, mid - 1));
-	}
-	else
-	{
-		res = calc_sqrt(n, mid + 1, end);
-		return ((res == -1) ? -1 : res);
-	}
+	return (calc_sqrt(n, start + 1));
 }
