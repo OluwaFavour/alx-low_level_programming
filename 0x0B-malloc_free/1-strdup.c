7190e1bfbd1828b3aa917c9e5bcc932e0f3403da
OluwaFavour <fsticks8187@gmail.com>
2023-03-14 08:20:11 +0000

    Add 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: The string to compute
+  *
+  * Return: Returns the length of the string
+  */
+int _strlen(char *s)
+{
+	char *p = s;
+
+	while (*p)
+		p++;
+	return ((int)(p - s));
+}
+
+/**
+  * _strdup - returns a pointer to a newly allocated space in memory
+  * which contains a copy of the string given as a parameter
+  *
+  * @str: String to copy
+  *
+  * Return: Returns a pointer to the new memory (Success), and NULL (Fail)
+  */
+char *_strdup(char *str)
+{
+	char *res;
+	int i, len;
+
+	if (str == NULL)
+		return (NULL);
+	len = _strlen(str);
+	res = malloc(len * sizeof(char));
+	if (res == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i < len)
+	{
+		res[i] = str[i];
+		i++;
+	}
+
+	return (res);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,4 +3,6 @@
 
 int _putchat(char c);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+int _strlen(char *s);
 #endif /* MAIN */
