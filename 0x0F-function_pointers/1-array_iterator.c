17da21ced2d9e41d4cfa357fff41e2c2f2c7d793
OluwaFavour <fsticks8187@gmail.com>
2023-03-22 11:33:24 +0000

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,6 +13,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i = 0;
 
-	for (; i < size; i++)
-		action(array[i]);
+	if (array != NULL && size > 0)
+	{
+		for (; i < size; i++)
+			action(array[i]);
+	}
 }
