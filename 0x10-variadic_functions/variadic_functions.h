122777fb5f329ed9ac128312a2dd05f65cb7ffbc
Favour Olusayo <64292176+OluwaFavour@users.noreply.github.com>
2023-03-23 14:18:42 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,4 +6,19 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *seperator, const unsigned int n, ...);
 void print_strings(const char *seperator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
+void print_string(va_list args);
+
+/**
+ * struct format_printer - struct for format printer
+ * @format: format to print
+ * @print: function to print
+ */
+struct format_printer
+{
+	char format;
+	void (*print)(va_list args);
+};
 #endif /* VARIADIC_FUNCTIONS_H */
