9fda0e97ebd4f71663354db07257dc3ca0f49158
OluwaFavour <fsticks8187@gmail.com>
2023-03-30 22:26:30 +0000

    Add 1-list_len.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-int _putchar(char c);
 /* Type definitions */
 /**
  * struct list_s - singly linked list
@@ -17,5 +16,8 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
+int _putchar(char c);
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 #endif /* LISTS_H */
