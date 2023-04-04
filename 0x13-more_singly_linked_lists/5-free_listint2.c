2581f88bdd777452a86b46ee188544a9a5c8cd3a
OluwaFavour <fsticks8187@gmail.com>
2023-04-04 17:44:21 +0000

    Add 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current_node;
 
+	if (head == NULL)
+		return;
+
 	while (*head != NULL)
 	{
 		current_node = *head;
 		*head = (*head)->next;
 		free(current_node);
 	}
+
+	*head = NULL;
 }
