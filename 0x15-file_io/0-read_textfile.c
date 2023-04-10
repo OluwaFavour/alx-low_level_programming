837e0ec267e5f75c2b4244f9cdafcc99d6cb47f5
OluwaFavour <fsticks8187@gmail.com>
2023-04-10 10:31:51 +0000

    Add 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include "main.h"
+
+/**
+  * read_textfile - Reads a text file and prints it to the standard output
+  *
+  * @filename: The text file to read
+  * @letters: Number of letters to be read and printed
+  *
+  * Return: Returns the number of letters actually read and printed (Success)
+  * or 0 (Fail)
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int filedes;
+	char *buffer;
+	ssize_t lettersRead = 0;
+	ssize_t lettersPrinted = 0;
+
+	if (filename == NULL)
+		return (0);
+	/* Open File */
+	filedes = open(filename, O_RDONLY);
+	if (filedes == -1)
+		return (0);
+	/* Read file to buffer */
+	buffer = malloc(letters);
+	lettersRead = read(filedes, buffer, letters);
+	if (lettersRead == -1)
+		return (0);
+	/* Write buffer to standard output */
+	lettersPrinted = write(STDOUT_FILENO, buffer, letters);
+	free(buffer);
+	if (lettersPrinted == -1)
+		return (0);
+
+	return (lettersPrinted);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,4 +2,5 @@
 #define MAIN_H
 
 int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
 #endif /* MAIN_H */
