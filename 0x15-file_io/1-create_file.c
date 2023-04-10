7917945011ab0d74077cc2edd9bd584a6825e2d0
OluwaFavour <fsticks8187@gmail.com>
2023-04-10 11:53:33 +0000

    Add 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,58 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+
+int _strlen(char *s);
+/**
+  * create_file - Creates a file
+  *
+  * @filename: Name of the file to create and write to
+  * @text_content: NULL terminated string to write to the created file
+  *
+  * Return: Returns 1 (Success), or -1 (Fail)
+  */
+int create_file(const char *filename, char *text_content)
+{
+	int filedes;
+	ssize_t bytesWritten = 0;
+	mode_t permissions = S_IRUSR | S_IWUSR; /* rw------- */
+
+	if (filename == NULL)
+		return (-1);
+	/* Open/create file */
+	filedes = open(filename, O_CREAT | O_WRONLY | O_TRUNC, permissions);
+	if (filedes == -1)
+		return (-1);
+	/* Write to file */
+	if (text_content != NULL)
+	{
+		bytesWritten = write(
+				filedes,
+				text_content,
+				(_strlen(text_content) + 1)
+		); /* +1 to add null terminator to file */
+		if (bytesWritten == -1)
+		{
+			close(filedes);
+			return (-1);
+		}
+	}
+	if (close(filedes) == -1)
+		return (-1);
+	return (1);
+}
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: The string to compute
+  *
+  * Return: Returns the length of the string
+  */
+int _strlen(char *s)
+{
+	char *p = s;
+
+	while (*p)
+		p++;
+	return ((int)(p - s));
+}
