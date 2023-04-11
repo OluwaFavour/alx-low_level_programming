b9507ccbab490f52d086e2e1c30ac1d8a593e1ec
OluwaFavour <fsticks8187@gmail.com>
2023-04-11 12:23:15 +0000

    Add 102-print_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+size_t listint_len_safe(const listint_t *head);
+/**
+  * free_listint_safe - Frees a listint_t list even with loops
+  *
+  * @h: A pointer to a pointer to the head node
+  *
+  * Return: Returns the size of the list that was free'd
+  */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *current_node;
+	size_t index, count;
+
+	count = listint_len_safe(*h);
+
+	if (count == 0)
+	{
+		while (*h != NULL)
+		{
+			current_node = *h;
+			*h = (*h)->next;
+			free(current_node);
+		}
+		return (sizeof(*h));
+	}
+
+	for (index = 0; index < count && *h != NULL; index++)
+	{
+		current_node = *h;
+		*h = (*h)->next;
+		free(current_node);
+	}
+
+	*h = NULL;
+	return (sizeof(*h));
+}
