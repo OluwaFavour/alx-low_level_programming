373eb3b9fc4234ecac40370b86b5e4478887c88f
OluwaFavour <fsticks8187@gmail.com>
2023-05-08 22:51:48 +0000

    Add 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -36,7 +36,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	len++;
 	nwritten = write(fd, text_content, len);
-	if (nwritten < len)
+	if (nwritten == -1)
 		return (-1);
 	if (close(fd) == -1)
 		return (-1);
