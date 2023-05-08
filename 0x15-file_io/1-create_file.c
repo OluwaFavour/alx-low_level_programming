90df565647296f8db8bb2481d9c9b3cc99dea677
OluwaFavour <fsticks8187@gmail.com>
2023-05-08 23:07:17 +0000

    Add 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,43 +2,53 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+int _strlen(char *s);
 /**
-  * create_file - Creates a file
-  *
-  * @filename: The name of the file to create
-  * @text_content: A NULL terminated string to write to the file
-  *
-  * Return: Returns 1 (Success), or -1 (Failure)
-  */
+ * create_file - Creates a file
+ *
+ * @filename: The name of the file to create
+ * @text_content: A NULL terminated string to write to the file
+ *
+ * Return: Returns 1 (Success), or -1 (Failure)
+ */
 int create_file(const char *filename, char *text_content)
 {
-	char *text;
-	int fd, len;
-	ssize_t nwritten;
+	int filedes;
+	ssize_t bytesWritten = 0;
+	mode_t permissions = S_IRUSR | S_IWUSR; /* rw------- */
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
+	/* Open/create file */
+	filedes = open(filename, O_CREAT | O_WRONLY | O_TRUNC, permissions);
+	if (filedes == -1)
 		return (-1);
-	if (text_content == NULL)
+	/* Write to file */
+	if (text_content != NULL)
 	{
-		if (close(fd) == -1)
+		bytesWritten = write(filedes, text_content, _strlen(text_content));
+		if (bytesWritten == -1)
+		{
+			close(filedes);
 			return (-1);
-		return (1);
+		}
 	}
-	text = text_content;
-	len = 0;
-	while (*text != '\0')
-	{
-		len++;
-		text++;
-	}
-	len++;
-	nwritten = write(fd, text_content, len);
-	if (nwritten < len)
-		return (-1);
-	if (close(fd) == -1)
+	if (close(filedes) == -1)
 		return (-1);
 	return (1);
+}
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: The string to compute
+ *
+ * Return: Returns the length of the string
+ */
+int _strlen(char *s)
+{
+	char *p = s;
+
+	while (*p)
+		p++;
+	return ((int)(p - s));
 }
